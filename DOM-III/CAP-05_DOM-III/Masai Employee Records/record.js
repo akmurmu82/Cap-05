bc656b29d72253fc3a43f414908df80878fda5e8
akmurmu82 <akmurmu83@gmail.com>
2023-09-07 15:08:36 +0530

    CSS fixed to MER

--- DOM-III/CAP-05_DOM-III/Masai Employee Records/record.js
+++ DOM-III/CAP-05_DOM-III/Masai Employee Records/record.js
@@ -12,18 +12,25 @@ function submitData(event) {
     let email = form.email.value;
     let mbl = form.mbl.value;
 
-    createRow(name, employeeID, department, exp, email, mbl);
+    let role = exp<=1 ? 'Fresher': exp>=2 && exp<=5 ? 'Junior': exp>5 ? 'Senior': null;
+
+    createRow(name, employeeID, department, exp, email, mbl, role, 'Delete');
 }
 
-function createRow(name, empId, dep, exp, email, mob) {
-    let paramArr = [name, empId, dep, exp, email, mob];
+function createRow(name, empId, dep, exp, email, mob, role, del) {
+    let paramArr = [name, empId, dep, exp, email, mob, role, del];
     let tr = document.createElement('tr');
     
+    tr.addEventListener('click', (event)=> {
+        event.target.parentNode.remove();
+    })
+    
     for(let param of paramArr) {
+        if(param == role) {
+        }
         let td = document.createElement('td');
         td.innerText = param;
         tr.append(td);
     }
-    console.log(tr, tbody)
     tbody.append(tr);
 }
