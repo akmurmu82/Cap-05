d74b6531575bba5be6b395db34168af3c4030aee
akmurmu82 <akmurmu83@gmail.com>
2023-09-25 12:11:33 +0530

    phonePay

--- CAP-05 Promises/PHONEPE/index.js
+++ CAP-05 Promises/PHONEPE/index.js
@@ -1,7 +1,46 @@
-let form = document.querySelector('form')
-let pendingScreen = document.querySelector('pendingScreen')
-let home = document.querySelector('home')
+let form = document.querySelector('#form')
+let pendingScreen = document.querySelector('#pendingScreen')
+let successfulScreen = document.querySelector('#successfulScreen')
+let failureScreen = document.querySelector('#failedScreen')
+let home = document.querySelector('#home')
 
 
-let inputAmount = document.querySelector('inputAmount').value
-let submitBtn = document.querySelector('submitBtn')
+let inputAmount = document.querySelector('#inputAmount')
+let submitBtn = document.querySelector('#submit')
+
+let ranNum = Math.floor(Math.random() * 10); // generating a random number
+console.log(ranNum);
+
+
+function showSuccess() {
+    pendingScreen.style.display = 'none'
+    successfulScreen.style.display = 'block'
+}
+
+function showFailure() {
+    pendingScreen.style.display = 'none'
+    failureScreen.style.display = 'block'
+}
+
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log(inputAmount.value);
+    // toggling to the pending screen
+    pendingScreen.style.display = 'block'
+    home.style.display = 'none'
+
+    let myPromise = new Promise((res, rej) => {
+        setTimeout(() => {
+            ranNum % 2 == 0 ? res(showSuccess) : rej(showFailure); // resolving/rejecting a/c wherether the randNum is even/odd
+        }, 2000);
+    })
+
+    myPromise
+        .then((showSuccess)=> {
+            showSuccess()
+        })
+        .catch((showFailure)=>{
+            showFailure()
+        })
+})
+
