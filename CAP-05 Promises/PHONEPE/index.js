65917a7c991c812368aebee4960c75894db9c120
akmurmu82 <akmurmu83@gmail.com>
2023-09-25 11:15:43 +0530

    async assignment is done

---
+++ CAP-05 Promises/PHONEPE/index.js
@@ -0,0 +1,7 @@
+let form = document.querySelector('form')
+let pendingScreen = document.querySelector('pendingScreen')
+let home = document.querySelector('home')
+
+
+let inputAmount = document.querySelector('inputAmount').value
+let submitBtn = document.querySelector('submitBtn')
